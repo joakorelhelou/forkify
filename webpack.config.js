6e79d32c1abc44a44c74b2316578daa8d53282b3
joakorelhelou <joako.33k@gmail.com>
2018-04-16 10:58:58 -0300

    Adding babel config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: './src/js/index.js', //Where the bundle starts from
+    entry: ['babel-polyfill', './src/js/index.js'], //Where the bundle starts from. Polyfill to implemen tpromises in IE5
     output: { //path and name of the bundle file
         path: path.resolve(__dirname, 'dist'), //This path is a node package, __dirname is accessible thanks to node
         filename: 'js/bundle.js'
@@ -15,5 +15,16 @@ module.exports = {
             filename: 'index.html',
             template: './src/index.html'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            { //Test all js files and apply babel loader to transform as files
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 };
